dba469c42ea4d74ba318cbe2ad298c661f58306b
carloskanira <carloskanira6@gmail.com>
2026-04-09 23:45:36 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,14 @@
+def addtwonumber():
+    num=int(input("enter first number:"))
+    num2=int(input("enter second number:"))
+    print(f"the sum of{num} and {num2} is {num+num2}")
+    
+addtwonumber()
+
+def mult():
+    num1=int(input("enter first number:"))
+    num2=int(input("enter second number:"))
+    print(f"the product of {num1} and {num2} is {num1*num2}")
+    
+
+mult()
